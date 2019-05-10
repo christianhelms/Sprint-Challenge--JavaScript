abbcf5f49ba2396920bb918e4be6c4cebc8e2be1
christianhelms <fonticons-dev@Fonticonss-MBP.hitronhub.home>
2019-05-10 13:05:40 -0500

    finished

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(x, y, callback) {
+  return callback(x, y)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +17,27 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(x, y, add) {
+  sum = x + y
+  console.log(sum);
+  return sum
+}
+
+function multiply(x, y, multiply) {
+  sum = x * y
+  console.log(sum);
+  return sum
+}
+
+function greeting(x, y, greeting) {
+  return `Hello ${x} ${y}, nice to meet you!`
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +45,8 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+// A: because it's nested withing the function so it has access to that scope. If it weren't nested, 
+// it wouldn't have the same permissions that it does.
 
 
 const external = "I'm outside the function";
